70b70f2abc7433343f1d456cf98dc9c45cc3841a
Rohit Shah <shahrohit2223@gmail.com>
2026-08-26 21:51:08 +0545

    level 2

--- 15-Generic-Function/index.js
+++ 15-Generic-Function/index.js
@@ -1,12 +1,61 @@
 "use strict";
-//generic function
+// // //generic function
 Object.defineProperty(exports, "__esModule", { value: true });
-// function getValue(value:number):number{
+// // // function getValue(value:number):number{
+// // //     return value;
+// // // }
+// function getValue<T>(value:T):T{
 //     return value;
 // }
-function getValue(value) {
-    return value;
+// // console.log(getValue<number>(10))
+// // console.log(getValue<string>("abc"))
+// // getValue<number>(100);
+// // getValue<string>("hello");
+// const user={
+//     id:1,
+//     name:"Alex"
+// }
+// const result=getValue(user)
+// console.log(result.id)
+// console.log(result.name)
+// const numbers=[10,20,30];
+// const res=getValue(numbers)
+// const names=["Alex","Ani"]
+// const resultString=getValue(names);
+// res.push(40)
+// console.log(res)
+// console.log(resultString)
+//mulitiple generic types
+// function makePair<T,U>(a:T,b:U):[T,U]{
+//     return [a,b];
+// }
+// const pair=makePair(25,"Alex");
+// const age:number=pair[0];
+// const name:string=pair[1]
+// console.log(age,name)
+//constraint
+// function getLength<T extends {length: number}>(value:T):number{
+//     return value.length;
+// }
+// console.log(getLength("Hello"))
+// console.log(getLength([10,20,30]))
+// console.log(getLength(123));
+// function getName<T extends {name:string}>(obj:T):string{
+//     return obj.name;
+// }
+// const user={
+//     name:'Alex',
+//     age:20
+// }
+// console.log(getName(user))
+function getProperty(obj, key) {
+    return obj[key];
 }
-console.log(getValue(10));
-console.log(getValue("abc"));
+const user = {
+    name: 'Alex',
+    age: 20
+};
+console.log(getProperty(user, "name"));
+console.log(getProperty(user, "age"));
+// console.log(getProperty(user,"email"));
 //# sourceMappingURL=index.js.map
--- 15-Generic-Function/index.ts
+++ 15-Generic-Function/index.ts
@@ -1,13 +1,96 @@
-//generic function
+// // //generic function
 
 
-// function getValue(value:number):number{
+// // // function getValue(value:number):number{
+// // //     return value;
+// // // }
+
+
+// function getValue<T>(value:T):T{
 //     return value;
+// }
+// // console.log(getValue<number>(10))
+// // console.log(getValue<string>("abc"))
+
+
+// // getValue<number>(100);
+// // getValue<string>("hello");
+
+
+
+// const user={
+//     id:1,
+//     name:"Alex"
+// }
+
+// const result=getValue(user)
+
+// console.log(result.id)
+// console.log(result.name)
+
+
+// const numbers=[10,20,30];
+
+// const res=getValue(numbers)
+
+// const names=["Alex","Ani"]
+// const resultString=getValue(names);
+// res.push(40)
+// console.log(res)
+// console.log(resultString)
+
+
+
+//mulitiple generic types
+// function makePair<T,U>(a:T,b:U):[T,U]{
+//     return [a,b];
+// }
+
+// const pair=makePair(25,"Alex");
+
+
+// const age:number=pair[0];
+// const name:string=pair[1]
+// console.log(age,name)
+
+
+//constraint
+
+// function getLength<T extends {length: number}>(value:T):number{
+//     return value.length;
 // }
 
 
-function getValue<T>(value:T):T{
-    return value;
+// console.log(getLength("Hello"))
+// console.log(getLength([10,20,30]))
+// console.log(getLength(123));
+
+
+// function getName<T extends {name:string}>(obj:T):string{
+//     return obj.name;
+// }
+
+// const user={
+//     name:'Alex',
+//     age:20
+// }
+
+// console.log(getName(user))
+
+
+
+
+
+function getProperty<T ,K extends keyof T>(obj:T,key:K):T[K]{
+        return obj[key];
 }
-console.log(getValue(10))
-console.log(getValue("abc"))
+
+const user={
+    name:'Alex',
+    age:20
+}
+
+
+console.log(getProperty(user, "name"));
+console.log(getProperty(user, "age"));
+// console.log(getProperty(user,"email"));
