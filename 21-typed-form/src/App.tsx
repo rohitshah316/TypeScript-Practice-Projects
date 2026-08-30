fee9547f2af1643888cb66a04dc60c2fe94b9be2
Rohit Shah <shahrohit2223@gmail.com>
2026-08-30 12:59:47 +0545

    level 3

--- 21-typed-form/src/App.tsx
+++ 21-typed-form/src/App.tsx
@@ -1,43 +1,86 @@
-import React, { useState } from "react";
+import { useState, type ChangeEvent, type FormEvent } from "react";
+
+type FormData={
+  name:string;
+  email:string;
+}
+
+type FormErrors={
+  name?:string;
+  email?:string;
+}
+
+type FormField=keyof FormData;
 
 const App = () => {
+  // const [name,setName]=useState("");
+  // const [email,setEmail]=useState("");
 
-  const [name,setName]=useState("");
-  const [email,setEmail]=useState("");
+  const [formData,setFormData]=useState<FormData>({
+    name:"",
+    email:"",
+  })
 
-  const handleNameChange=(
-    e: React.ChangeEvent<HTMLInputElement>
-  )=>{
-    setName(e.target.value);
+  const[errors,setErrors]=useState<FormErrors>({});
+
+
+  const handleChange=(e:ChangeEvent<HTMLInputElement>)=>{
+
+    const field=e.target.name as FormField;
+    setFormData({...formData,[field]: e.target.value })
   }
 
-  const handleEmailChange=(
-    e:React.ChangeEvent<HTMLInputElement>
-  )=>{
-    setEmail(e.target.value);
-  }
 
-  const handleSubmit=(
-    e:React.FormEvent
-  )=>{
+  const handleSubmit=(e:FormEvent<HTMLFormElement>)=>{
     e.preventDefault();
 
-    console.log(name);
-    console.log(email);
+    const newErrors: FormErrors={};
+
+    if(!formData.name.trim()){
+      newErrors.name="Name is required";
+    }
+
+
+    if(!formData.email.trim()){
+      newErrors.email="Email is required"
+    }
+    if(formData.email.trim() && !formData.email.includes("@")){
+      newErrors.email="Enter a valid email";
+    }
+
+    setErrors(newErrors);
+    console.log(newErrors)
+    console.log(formData)
   }
+  // const handleNameChange=(e:ChangeEvent<HTMLInputElement>)=>{
+  //   setName(e.target.value)
+  // }
+
+  // const handleEmailChange=(e:ChangeEvent<HTMLInputElement>)=>{
+  //   setEmail(e.target.value)
+  // }
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text"
-        value={name}
-        onChange={handleNameChange}
-      />
-      <input type="email"
-        value={email}
-        onChange={handleEmailChange}
-      />
+      <h1>User Form</h1>
 
-  <button type="submit">Submit</button>
+      <input type="text" 
+      value={formData.name}
+      name="name"
+      onChange={handleChange}
       
+      />
+      {errors.name && <p>{errors.name}</p>}
+      <input type="email"
+      name="email"
+      value={formData.email}
+      onChange={handleChange}
+      
+      />
+{errors.email && <p>{errors.email}</p>}
+      <p>Name: {formData.name}</p>
+      <p>Email: {formData.email}</p>
+
+      <button type="submit">Submit</button>
     </form>
   )
 }
